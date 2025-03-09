7d87e1d03849c7296262533fbefb00f32d4c90e7
mayurthorat74 <113770930+mayur74@users.noreply.github.com>
2025-03-09 12:30:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome SubStrings/palindrome-sub-strings.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+  
+  int palinCount(int left , int right , string &s){
+      int count = 0;
+      while(left>=0 && right<s.size()){
+          if(s[left] == s[right]){
+              count++;
+              left--;
+              right++;
+          }
+          else break;
+      }
+      return count;
+  }
+    int countPS(string &s) {
+        // code here
+        int n = s.size();
+        int count = 0;
+        
+        for(int i = 0; i<n; i++){
+            count += palinCount(i-1, i+1, s);
+            
+        }
+        for(int i= 1; i<n; i++){
+            count +=palinCount(i-1 , i , s);
+        }
+        return count;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.countPS(s) << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
