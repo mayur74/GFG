46a646af144d0202a45432783811ef8adb5cdb65
mayurthorat74 <113770930+mayur74@users.noreply.github.com>
2025-07-11 11:22:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Trail of ones/trail-of-ones.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int countConsec(int n) {
+        // code here
+        if(n==2) return 1;
+        int prev= 0;
+        int curr=1;
+        int next;
+        int ans=1;
+        
+        for(int i =3; i<=n;i++){
+            next = prev+curr;
+            ans= ans*2+next;
+            prev=curr;
+            curr= next;
+        }
+        return ans;
+    }
+};
