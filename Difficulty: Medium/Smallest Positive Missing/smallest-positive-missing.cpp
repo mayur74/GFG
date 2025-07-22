dae9adf70cc58ddc9ce8b80a4235d7220379fdd2
mayurthorat74 <113770930+mayur74@users.noreply.github.com>
2025-07-22 23:27:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int missingNumber(vector<int> &arr) {
+        // code here
+        vector<bool> v(1000010, 0);
+        for(int x:arr)
+        if(x>0) v[x] = 1;
+        for(int i =1; i<100000; ++i)
+        if(v[i]==0) return i;
+        return -1;
+    }
+};
